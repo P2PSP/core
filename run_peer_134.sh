c1dfcc74f3b2595c2ab8ccd75b9b1659948d32ff
cristobalmedinalopez@gmail.com <>
2012-09-16 17:15:02 +0200

    run_peer_134 restore to trunk version

--- run_peer_134.sh
+++ run_peer_134.sh
@@ -1,36 +1,46 @@
-#!/usr/bin/python
-# -*- coding: iso-8859-15 -*-
+#!/bin/sh
 
-# No solicita retransmisión de bloques perdidos. Avisa al nodo fuente
-# de los peers eliminados de la lista de peers. Posee un buffer para
-# acomodar el jitter.
+source=150.214.150.68:4552
+peer_port=9999
 
-# {{{ Imports
+usage() {
+    echo $0
+    echo "  [-s (source end-point, $source by default)]"
+    echo "  [-l (listining peer port, $peer_port by defaults)]"
+    echo "  [-? (help)]"
+}
 
-import getopt
-import sys
-import socket
-from blocking_socket import blocking_socket
-from colors import Color
-import struct
+echo $0: parsing: $@
 
-# }}}
+while getopts "s:l:?" opt; do
+    case ${opt} in
+	s)
+	    source="${OPTARG}"
+	    ;;
+	l)
+	    peer_port="${OPTARG}"
+	    ;;
+	?)
+	    usage
+	    exit 0
+	    ;;
+	\?)
+	    echo "Invalid option: -${OPTARG}" >&2
+	    usage
+	    exit 1
+	    ;;
+	:)
+	    echo "Option -${OPTARG} requires an argument." >&2
+	    usage
+	    exit 1
+	    ;;
+    esac
+done
 
-IP_ADDR = 0
-PORT = 1
+#xterm -e "./peer.py -s $source -l $peer_port" &
+./peer.py -s $source -l $peer_port -b 32 &
 
-source_name = "150.214.150.68"
-source_port = 4552
-player_port = 9999
-peer_port = 0 # OS default behavior will be used for port binding
-buffer_size = 32
+sleep 1
 
-def usage():
-    # {{{
-
-    print "This is " + sys.argv[0] + ", the peer node of a P2PSP network"
-    print
-    print "Parameters (and default values):"
-    print
-    print " -[-b]uffer_size=size of the peer buffer in blocks (" + str(buffer_size) + ")"
-    pri
+#firefox http://localhost:$peer_port &
+vlc http://localhost:$peer_port > /dev/null 2> /dev/null &
