bb0f227adf4c5109867155014dd421e342a57b88
Prince <prince.mst13@itbhu.ac.in>
2015-06-03 20:21:36 +0530

    player/status box hidden on player fullscreen

--- src/p2psp_application_gui.py
+++ src/p2psp_application_gui.py
@@ -25,12 +25,14 @@ class Main_Application():
     PLAYER_MRL = 'http://localhost:9999'
     Peer_IMS.USE_LOCALHOST = True 
     PLAYER_MEDIA_SOURCE = "./gui/p2psp.jpg"
-    PEER_ACTIVE  = False
-    PLAYER_PAUSED = False
-    PLAYER_FULLSCREEN  = False
-    CHANNELS_REVEALED = True
+    
     
     def __init__(self):
+        self.peer_active  = False
+        self.player_paused = False
+        self.player_fullscreen  = False
+        self.channels_revealed = True
+        self.status_box_hidden = False
         self.interface = gui.file_util.get_user_interface('gui', 'gui/P2PSP_GUI.glade')
         self.load_widgets()
         self.interface.connect_signals(self.setup_signals())
@@ -54,7 +56,7 @@ class Main_Application():
     def start_peer(self):
         thread1 = Peer_Thread(1, "Peer Thread")
         thread1.start()
-        self.PEER_ACTIVE = True
+        self.peer_active = True
         print 'thread started'
         
     def show(self):
@@ -66,7 +68,7 @@ class Main_Application():
         ,'on_TogglePlaybackButton_clicked'      : self.toggle_player_playback
         ,'on_ToggleChannels_button_press_event' : self.toggle_channel_box
         ,'on_FullscreenButton_clicked'          : self.toggle_player_fullscreen
-        ,'on_Surface_motion_notify_event' : self.toggle_status_box
+        ,'on_Surface_button_press_event'        : self.toggle_status_box
                    }
         return signals
     
@@ -74,12 +76,11 @@ class Main_Application():
         self.player_surface.set_size_request(600,600)
         self.player_surface.show()
         self.player_surface.connect("realize",self._realized)
-        '''self.player_surface.set_events(self.player_surface.get_events() |
-                               Gdk.EventMask.POINTER_MOTION_HINT_MASK |
-                               Gdk.EventMask.POINTER_MOTION_MASK)'''
+        self.player_surface.set_events(self.player_surface.get_events() |
+                                       Gdk.EventMask.BUTTON_PRESS_MASK)
         
     def toggle_player_type(self,win_id):
-        if self.PEER_ACTIVE :
+        if self.peer_active :
             self.player = self.player_instance.stream_player(self.win_id,self.PLAYER_MRL)
         else:
             self.player = self.player_instance.media_player(self.win_id,self.PLAYER_MEDIA_SOURCE)
@@ -87,7 +88,7 @@ class Main_Application():
              
     def stop_player(self, widget, data=None):
         self.player.stop()
-        self.PEER_ACTIVE = False
+        self.peer_active = False
         self.toggle_player_type(self.win_id)
         self.playback_toggle_button.set_image(self.play_image)
         #self.player_instance.em.event_attach(gui.vlc.EventType.MediaPlayerEndReached,self.end_callback)
@@ -96,67 +97,71 @@ class Main_Application():
         #self.player_instance.em.event_detach(gui.vlc.EventType.MediaPlayerEndReached)
         self.player.stop()
         
-    def end_callback(self,event):
+    '''def end_callback(self,event):
         print('End of media stream (event %s)' % event.type)
-        if self.PEER_ACTIVE == False:
-            self.toggle_player_type(self.win_id)
+        if self.peer_active == False:
+            self.toggle_player_type(self.win_id)'''
         
     def toggle_player_playback(self, widget, data=None):
-        if self.PEER_ACTIVE == False and self.PLAYER_PAUSED == False:
+        if self.peer_active == False and self.player_paused == False:
             #self.player_instance.em.event_detach(gui.vlc.EventType.MediaPlayerEndReached)
             self.start_peer()
             self.playback_toggle_button.set_image(self.pause_image)
             self.toggle_player_type(self.win_id)
             
-        elif self.PEER_ACTIVE == True and self.PLAYER_PAUSED == True:
+        elif self.peer_active == True and self.player_paused == True:
             self.player.play()
             self.playback_toggle_button.set_image(self.pause_image)
-            self.PLAYER_PAUSED = False
-        elif self.PEER_ACTIVE == True and self.PLAYER_PAUSED == False:
+            self.player_paused = False
+        elif self.peer_active == True and self.player_paused == False:
             self.player.pause()
             self.playback_toggle_button.set_image(self.play_image)
-            self.PLAYER_PAUSED = True
+            self.player_paused = True
         else:
             pass
         
     def toggle_channel_box(self, widget, data=None):
-        if self.CHANNELS_REVEALED == True:
+        if self.channels_revealed == True:
             self.channel_box.hide()
-            self.channel_revealer.set_label('>>')
-            self.CHANNELS_REVEALED = False
-        elif self.CHANNELS_REVEALED == False:
-            self.channel_box.show()
             self.channel_revealer.set_label('<<')
-            self.CHANNELS_REVEALED = True
+            self.channels_revealed = False
+        elif self.channels_revealed == False:
+            self.channel_box.show()
+            self.channel_revealer.set_label('>>')
+            self.channels_revealed = True
         
     def hide_all_but_surface(self):
         self.menu.hide()
         self.channel_box.hide()
-        #self.status_box.hide()
+        self.status_box.hide()
         
     def toggle_player_fullscreen(self, widget, data=None):
-        if self.PLAYER_FULLSCREEN == False:
+        if self.player_fullscreen == False:
             self.window.fullscreen()
             self.hide_all_but_surface()
-            self.PLAYER_FULLSCREEN = True
+            self.player_fullscreen = True
+            self.status_box_hidden = True
         else:
             self.window.unfullscreen()
             self.show()
-            self.PLAYER_FULLSCREEN = False
+            self.player_fullscreen = False
+            self.status_box_hidden = False
             
     def hide_status_box(self):
         self.status_box.hide()
-        print 'hidden'
         
     def show_status_box(self):
         self.status_box.show()
-        print 'shown'
         
     def toggle_status_box(self,widget,data=None):
-        '''if self.PLAYER_FULLSCREEN == True:
-            print 'hidden'
-            '''
-                
+        if self.player_fullscreen == True:
+            if self.status_box_hidden == False :
+                self.hide_status_box()
+                self.status_box_hidden = True
+            else:
+                self.show_status_box()
+                self.status_box_hidden = False
+                                
     def _realized(self,widget,data=None):
         self.player_instance = Player_Instance()
         #self.player_instance.em.event_attach(gui.vlc.EventType.MediaPlayerEndReached,self.end_callback)
