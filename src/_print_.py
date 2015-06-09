7fcc6d66763c89b1470ccda67c0c20716a196893
vicente-gonzalez-ruiz <vicente.gonzalez.ruiz@gmail.com>
2015-06-09 20:43:03 +0200

    Adding thanks

--- src/_print_.py
+++ src/_print_.py
@@ -1,3 +1,7 @@
+# The P2PSP.org project has been supported by the Junta de Andalucía
+# through the Proyecto Motriz "Codificación de Vídeo Escalable y su
+# Streaming sobre Internet" (P10-TIC-6548).
+
 from __future__ import print_function
 from time import gmtime, strftime
 
