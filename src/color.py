dc3b6f828b29ede42152e171320aca9bd4bed005
vicente-gonzalez-ruiz <vicente.gonzalez.ruiz@gmail.com>
2015-06-15 07:43:37 +0200

    Removing thanks

--- src/color.py
+++ src/color.py
@@ -1,9 +1,5 @@
 # Colorized printing
 
-# The P2PSP.org project has been supported by the Junta de Andalucía
-# through the Proyecto Motriz "Codificación de Vídeo Escalable y su
-# Streaming sobre Internet" (P10-TIC-6548).
-
 class Color:
 
     none = '\033[0m'
