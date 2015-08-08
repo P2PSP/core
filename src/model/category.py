a1c2d52ef33003d11f5fdb1f870bb2c160f3c660
Prince <prince.mst13@itbhu.ac.in>
2015-08-08 18:47:54 +0530

    Model modules documented

--- src/model/category.py
+++ src/model/category.py
@@ -1,7 +1,32 @@
+"""
+@package model
+category module 
+"""
+# This code is distributed under the GNU General Public License (see
+# THE_GENERAL_GNU_PUBLIC_LICENSE.txt for extending this information).
+# Copyright (C) 2014, the P2PSP team.
+# http://www.p2psp.org
+
+# The P2PSP.org project has been supported by the Junta de Andalucia
+# through the Proyecto Motriz "Codificacion de Video Escalable y su
+# Streaming sobre Internet" (P10-TIC-6548).
 
 class Category():
-
+    
+    """
+    Store channels in different categories.
+    Data Structure used is a Dictionary.
+    """
+    
     def __init__(self,name):
+        
+        """
+        Initialise with category name and a empty dictionary.
+        
+        @param : name 
+                Category name
+        """
+        
         self.name = name
         self.channels = {}
 
@@ -15,9 +40,27 @@ class Category():
         return self.name
 
     def add(self,key,channel):
+        
+        """
+        Appends a new channel to the existing channels.
+        
+        @param : key 
+                key of the channel in dictionary
+        @param : channel
+                Channel Object
+        """
+        
         self.channels[key] = channel
 
     def remove(self,key):
+        
+        """
+        Removes a channel from  the existing channels.
+        
+        @param : key 
+                key of the channel in dictionary
+        """
+        
         del(self.channels[key])
 
     def get_channel(self,key):
--- src/model/channel.py
+++ src/model/channel.py
@@ -1,10 +1,41 @@
-class Channel():
+"""
+@package model
+channel module 
+"""
+# This code is distributed under the GNU General Public License (see
+# THE_GENERAL_GNU_PUBLIC_LICENSE.txt for extending this information).
+# Copyright (C) 2014, the P2PSP team.
+# http://www.p2psp.org
 
+# The P2PSP.org project has been supported by the Junta de Andalucia
+# through the Proyecto Motriz "Codificacion de Video Escalable y su
+# Streaming sobre Internet" (P10-TIC-6548).
+
+class Channel():
+    
+    """
+    Stores A particular channel's data or configuration.
+    """
+    
     def __init__(self,data):
+        
+        """
+        Initialise channel with given data.
+        """
+        
+        ## Channel name
         self.name = data["name"]
+        
+        ## Channel details
         self.description = data["description"]
+        
+        ## Channel thumbnail_url
         self.thumbnail_url = data["thumbnail_url"]
+        
+        ## Channel Splitter Address
         self.splitter_addr = data["splitter_addr"]
+        
+        ## Channel Splitter Port
         self.splitter_port = data["splitter_port"]
 
     def set_name(self,name):
