622feae46473dbb6800be439f565ed12e416c55e
Jorge García Hinestrosa <jorgegarciahinestrosa@gmail.com>
2015-11-09 12:47:22 +0100

    Add splitter.cpp (main file)

---
+++ src/splitter.cpp
@@ -0,0 +1,21 @@
+//
+//  splitter.cpp
+//  P2PSP
+//
+//  This code is distributed under the GNU General Public License (see
+//  THE_GENERAL_GNU_PUBLIC_LICENSE.txt for extending this information).
+//  Copyright (C) 2014, the P2PSP team.
+//  http://www.p2psp.org
+//
+
+int main(int argc, const char *argv[]) {
+  // TODO: Argument parser.
+
+  // TODO: Configure splitter
+
+  // TODO: Start the splitter's main thread
+
+  // TODO: Print information about the status of the splitter
+
+  return 0;
+}
