a3ed9f27e6ec2fcdf4df8dcd0d46b9ecf740823f
Antonio Vicente Martin <antoniovicente.avm@gmail.com>
2015-11-09 12:26:35 +0100

    Add peer main file

---
+++ src/peer.cpp
@@ -0,0 +1,18 @@
+
+// TODO: include all needed libraries
+
+int main(int argc, const char * argv[]) {
+  
+  // TODO: Argument parser. Decide how to implement it
+  
+  // TODO: Connect to the splitter to receive configuration before initialize any kind of peer.
+  
+  // TODO: Decide type of peer to work with
+  
+  // TODO: Start the peer's main thread
+  
+  // TODO: Print information about the status of the peer
+
+  return 0;
+}
+
