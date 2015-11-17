2fa3d2216a26d96df037cec954087537f49772a2
Jorge García Hinestrosa <jorgegarciahinestrosa@gmail.com>
2015-11-17 19:39:55 +0100

    Add splitter_dbs.h and splitter_dbs.cpp

---
+++ src/core/splitter_dbs.h
@@ -0,0 +1,18 @@
+//
+//  splitter_ims.h
+//  P2PSP
+//
+//  This code is distributed under the GNU General Public License (see
+//  THE_GENERAL_GNU_PUBLIC_LICENSE.txt for extending this information).
+//  Copyright (C) 2014, the P2PSP team.
+//  http://www.p2psp.org
+//
+//  DBS: Data Broadcasting Set of rules
+//
+
+#ifndef P2PSP_CORE_SPLITTER_DBS_H_
+#define P2PSP_CORE_SPLITTER_DBS_H_
+
+#include <stdio.h>
+
+#endif  // defined P2PSP_CORE_SPLITTER_DBS_H_
