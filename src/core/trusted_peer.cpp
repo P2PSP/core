c5e7a21ae87c8e391923042a93e34bc202e8e7c7
Antonio Vicente Martin <antoniovicente.avm@gmail.com>
2015-12-10 13:00:06 +0100

    Implement TrustedPeer::SendChunkHash()

--- src/core/trusted_peer.cpp
+++ src/core/trusted_peer.cpp
@@ -9,12 +9,24 @@ void TrustedPeer::Init() {
   LOG("Initialized");
 }
 void TrustedPeer::SetCheckAll(bool value) { check_all_ = value; };
-  
+
 int TrustedPeer::CalculateNextSampled() {
   if (check_all_) {
     return 0;
   }
   int max_random = (int)(peer_list_.size() / kSamplingEffort);
   return (int)((std::rand() % std::max(1, max_random)) + kPassNumber);
+};
+
+void TrustedPeer::SendChunkHash(int chunk_number) {
+  Chunk chunk = chunks_[chunk_number % buffer_size_];
+  // chunk_hash = hashlib.sha256(chunk).digest()
+
+  std::vector<char> msg(34);
+
+  *((short*)msg.data()) = htons((short)chunk_number);
+  // TODO: SHA256 library
+  // msg = struct.pack('H32s', chunk_number, chunk_hash)
+  team_socket_.send_to(buffer(msg), splitter_);
 };
 }
