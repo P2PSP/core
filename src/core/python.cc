32c5364d1abbbbf6ef2f01779b8af70a26610af3
jp_garcia_ortiz <jp.garcia.ortiz@gmail.com>
2015-12-22 09:23:02 +0000

    Begin of the Python interface

---
+++ src/core/python.cc
@@ -0,0 +1,20 @@
+#include <boost/python.hpp>
+
+#include "peer_ims.h"
+#include "peer_dbs.h"
+
+
+using namespace p2psp;
+using namespace boost::python;
+
+
+BOOST_PYTHON_MODULE(p2psp)
+{
+    class_<PeerIMS, boost::noncopyable>("PeerIMS")
+        .def("Init", &PeerIMS::Init)
+    ;
+    
+    class_<PeerDBS, boost::noncopyable>("PeerDBS")
+        .def("Init", &PeerDBS::Init)
+    ;
+}
