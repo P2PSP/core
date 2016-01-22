609347dfe1afe1addfb2d2e2a10fb3d23e0eab59
Antonio Vicente Martin <antoniovicente.avm@gmail.com>
2016-01-22 10:15:49 +0100

    Move main() in peer.cc to run() in  peer_core.h

---
+++ src/core/peer_core.h
@@ -0,0 +1,28 @@
+//
+//  peer_core.h
+//  P2PSP
+//
+//  This code is distributed under the GNU General Public License (see
+//  THE_GENERAL_GNU_PUBLIC_LICENSE.txt for extending this information).
+//  Copyright (C) 2016, the P2PSP team.
+//  http://www.p2psp.org
+//
+
+#include <boost/format.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include "common.h"
+#include "monitor_dbs.h"
+#include "monitor_lrs.h"
+#include "peer_dbs.h"
+#include "peer_ims.h"
+//#include "peer_strpeds.h"
+//#include "peer_strpeds_malicious.h"
+//#include "trusted_peer.h"
+//#include "malicious_peer.h"
+#include "../util/trace.h"
+
+namespace p2psp {
+
+int run(int argc, const char* argv[]);
+}
