f9f937092562ce789e315cc77cbca4c6c070cba4
Jorge García Hinestrosa <jorgegarciahinestrosa@gmail.com>
2016-01-26 13:15:30 +0100

    Remove the delay before launching the VLCPlayer

--- src/ios/p2psp-ios/p2psp-ios/ViewController.mm
+++ src/ios/p2psp-ios/p2psp-ios/ViewController.mm
@@ -6,15 +6,15 @@
 //  Copyright © 2016 P2PSP. All rights reserved.
 //
 
-#import "../../../core/peer_core.h"
 #import <MobileVLCKit/VLCMediaPlayer.h>
+#import "../../../core/peer_core.h"
 #import "ViewController.h"
 
-@interface ViewController () <VLCMediaPlayerDelegate>
-@property (weak, nonatomic) IBOutlet UITextField *tfSplitterAddr;
-@property (weak, nonatomic) IBOutlet UITextField *tfSplitterPort;
-@property (weak, nonatomic) IBOutlet UIButton *bPlay;
-@property (weak, nonatomic) IBOutlet UIView *subView;
+@interface ViewController ()<VLCMediaPlayerDelegate>
+@property(weak, nonatomic) IBOutlet UITextField *tfSplitterAddr;
+@property(weak, nonatomic) IBOutlet UITextField *tfSplitterPort;
+@property(weak, nonatomic) IBOutlet UIButton *bPlay;
+@property(weak, nonatomic) IBOutlet UIView *subView;
 
 @end
 
@@ -27,7 +27,7 @@ const NSString *splitterPort;
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
-  
+
   mediaPlayer = [[VLCMediaPlayer alloc] init];
   mediaPlayer.delegate = self;
   mediaPlayer.drawable = self.subView;
@@ -41,22 +41,21 @@ const NSString *splitterPort;
 - (IBAction)onPlay:(id)sender {
   splitterAddr = [self.tfSplitterAddr text];
   splitterPort = [self.tfSplitterPort text];
-  
-  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-    const char *kSplitterAddr = splitterAddr.UTF8String;
-    const char *kSplitterPort = splitterPort.UTF8String;
-    
-    const char *argv[] = {"p2psp", "--splitter_addr", kSplitterAddr,
-      "--splitter_port", kSplitterPort};
-    p2psp::run(5, argv);
-  });
-  
-  /*dispatch_async(dispatch_get_main_queue(), ^{*/
-   dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
-    mediaPlayer.media = [VLCMedia mediaWithURL:[NSURL URLWithString:@"http://localhost:9999"]];
-    
-    [mediaPlayer play];
-  });
+
+  dispatch_async(
+      dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        const char *kSplitterAddr = splitterAddr.UTF8String;
+        const char *kSplitterPort = splitterPort.UTF8String;
+
+        const char *argv[] = {"p2psp", "--splitter_addr", kSplitterAddr,
+                              "--splitter_port", kSplitterPort};
+        p2psp::run(5, argv);
+      });
+
+  mediaPlayer.media =
+      [VLCMedia mediaWithURL:[NSURL URLWithString:@"http://localhost:9999"]];
+
+  [mediaPlayer play];
 }
 
 @end
