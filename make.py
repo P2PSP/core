4afb87317a1b7c096c00b1b1dfecd509b51b0aff
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2016-03-30 23:14:56 +0200

    update make.py

--- make.py
+++ make.py
@@ -8,11 +8,14 @@ import platform
 bin_dir = 'bin'
 build_dir = 'build'
 only_cmake = False
+cmake = 'cmake'
 
 if len(sys.argv) >= 2:
     if sys.argv[1] == 'clean':
         os.system('git clean -dfx')
         quit()
+    if sys.argv[1] == 'debug':
+        cmake = cmake + ' -DCMAKE_BUILD_TYPE=Debug'
     elif sys.argv[1] == 'only-cmake':
         only_cmake = True
 
@@ -33,9 +36,11 @@ sys_name = platform.system()
 
 if sys_name == 'Linux':
     print '\nMaking for Linux...\n'
-    if os.system('cd build && cmake .. && echo') == 0:
+    command = 'cd build && ' + cmake + ' .. && echo'
+    if os.system(command) == 0:
+        command = 'cd build && make'
         if not only_cmake:
-            os.system('cd build && make')
+            os.system(command)
 
 elif sys_name == 'Darwin':
     print '\nMaking for OS X...\n'
