8ae873359cb82c416a7d16286d0f6fa6dfac8651
hehaichi <hehaichi@gmail.com>
2016-05-18 12:13:12 +0530

    Update tracker

--- src/core/tracker.py
+++ src/core/tracker.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 import socket
-
+class tracker():
+	def __init__(self):
+		self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		self.sock.bind(('',8000))
+		self.sock.listen(5)
