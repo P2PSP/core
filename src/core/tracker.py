89bfd333012e3ce59ee429167f36c79c7981e435
hehaichi <hehaichi@gmail.com>
2016-05-24 13:22:23 +0530

    Update tracker

--- src/core/tracker.py
+++ src/core/tracker.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 import socket
+import select
 class tracker():
 SPLITTER_LIST=[]
 PEER_LIST=[]
-	def __init__(self):
-		self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-		self.sock.bind(('',8000))
-		self.sock.listen(5)
+    def __init__(self):
+        self.sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.sock.bind(('',8000))
+        self.sock.listen(5)
 		
+		
+    def listen():
+        while 1:
+            read,write,error=select.select(SPLITTER_LIST[],[],[],0)
+            for sock in read:
+                if sock == self.sock:
+                    (sockfd,addr)=self.sock.accept()                    
+                    SPLITTER_LIST.append(sockfd)
+                
